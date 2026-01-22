f55917272060ad8a9095f6d275c41f7a156229e3
zMaxNery <nery.guilherme17@gmail.com>
2026-01-22 17:57:18 -0300

    ata

---
+++ src/Exercicios/App4-BtnShow.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import './App.css'
+
+function App() {
+
+  // Muda cores
+  const [ color, setColor] = useState("white");
+
+  const btnBlue = () =>{
+    if (color === "blue") {
+      colorWhite();
+    } else {
+      setColor("blue");
+    }
+  };
+
+  const btnRed = () =>{
+    if (color === "red") {
+      colorWhite();
+    } else {
+      setColor("red");
+    }
+  };
+
+  const colorWhite = () => {setColor("white")};
+
+  // Botão exibir
+  const [ show, setShow] = useState(false);
+
+  const isShow = () => { setShow(!show) };
+
+  return (
+    <div className="page">
+      <div className="card">
+        <button
+        onClick={isShow}
+        style={{
+          width: "90px",
+          height: "30px",
+          backgroundColor: "wheat"
+        }}>Exibir script</button>
+        { show && <div 
+          className="change-color"
+          style={{
+            borderStyle: "double",
+            borderColor: "black",
+            margin: "15px",
+          }}>
+          <div
+            style={{
+              margin: "12px",
+              width: "200px",
+              height: "200px",
+              backgroundColor: color
+            }}
+          ></div>
+          <button
+            onClick={btnBlue}
+            style={{
+              margin: "8px",
+              width: "100px",
+              height: "25px",
+              color: "white",
+              fontWeight: "bold",
+              backgroundColor: "blue",
+            }}
+          >Azul</button>
+          <button
+            onClick={btnRed}
+            style={{
+              width: "100px",
+              height: "25px",
+              color: "white",
+              fontWeight: "bold",
+              backgroundColor: "red",
+            }}
+          >Vermelho</button>
+        </div> }
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Exercicios/App5-Contador.jsx
@@ -0,0 +1,53 @@
+import { use, useState } from "react";
+import './App.css'
+
+function App() {
+  const [ counter, setCounter ] = useState(0);
+  
+  let color = "black";
+
+  const counterAdd = () => {
+    setCounter(counter + 1);
+  };
+
+  const counterRem = () => {
+    setCounter(counter - 1);
+  };
+
+  if (counter < 0) color = "red";
+  if (counter > 0) color = "green";
+
+  return (
+    <div className="page">
+      <div className="card">
+        <h1
+        style={{
+          color: color
+        }}>{counter}</h1>
+        <button
+        onClick={counterAdd}
+        style={{
+          margin: "10px",
+          width: "22px",
+          height: "22px",
+          color: "white",
+          fontWeight: "bold",
+          fontSize: "15px",
+          backgroundColor:"blueviolet"
+        }}>+</button>
+        <button
+        onClick={counterRem}
+        style={{
+          width: "22px",
+          height: "22px",
+          color: "white",
+          fontWeight: "bold",
+          fontSize: "15px",
+          backgroundColor:"blueviolet"
+        }}>-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
