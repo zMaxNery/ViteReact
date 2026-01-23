a45a7825e89b6edbee61f6947e99c18310218ac4
zMaxNery <nery.guilherme17@gmail.com>
2026-01-23 08:51:11 -0300

    SIM

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,11 @@ import { use, useState } from "react";
 import './App.css'
 
 function App() {
-  const [ user, setUser ] = useState({ name: "Visitante", status: "Offline" });
-
-  const [ btnLog, setBtnLog ] = useState(false);
-
-  const isBtnLog = () => { 
-    setBtnLog(!btnLog);
-    if (btnLog === true) setUser("Guilherme", "Online");
-  };  
+  const [ user, setUser ] = useState({ name: "Visitante", status: "Offline" }); 
 
   return (
     <div className="page">
       <div className="card">
-        <h2>Usuário: {user.name}</h2>
-        <h2>Status: {user.status}</h2>
-        { !btnLog && <button 
-          onClick={isBtnLog}
-          style={{
-            width: "60px",
-            height: "20px"
-          }}
-        >Entrar</button> }
-        { btnLog && <button 
-          onClick={isBtnLog}
-          style={{
-            width: "60px",
-            height: "20px"
-          }}
-        >Sair</button> }
       </div>
     </div>
   );
---
+++ src/Exercicios/App6-Login.jsx
@@ -0,0 +1,38 @@
+import { use, useState } from "react";
+import './App.css'
+
+function App() {
+  const [ user, setUser ] = useState({ name: "Visitante", status: "Offline" });
+
+  const isBtnLog = () => { 
+    if (user.name === "Visitante") {
+      setUser({ name: "Guilherme", status: "Online" });
+    } else {
+      setUser({ name: "Visitante", status: "Ofline" });
+    }
+  };  
+
+  return (
+    <div className="page">
+      <div className="card">
+        <h2>Usuário: {user.name}</h2>
+        <h2>Status: {user.status}</h2>
+        { user.name === "Visitante" ? <button 
+          onClick={isBtnLog}
+          style={{
+            width: "60px",
+            height: "20px"
+          }}
+        >Entrar</button> : <button 
+          onClick={isBtnLog}
+          style={{
+            width: "60px",
+            height: "20px"
+          }}
+        >Sair</button> }
+      </div>
+    </div>
+  );
+}
+
+export default App;
