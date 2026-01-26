e6649973da92f5e157441e33a100f676e490a742
zMaxNery <nery.guilherme17@gmail.com>
2026-01-26 15:30:50 -0300

    aaaaaaaaaaaaa

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,31 @@ const archive = import.meta.glob("./exercicios/*.jsx", { eager: true });
 function App() {
   const [ task, setTask ] = useState("");
 
+  const myTasks = {};
+
+  Object.keys(archive).forEach((obj) => {
+    const objDef = archive[obj].default;
+
+    const cleanName = obj.replace("./exercicios/", "");
+
+    myTasks[cleanName] = objDef;
+
+  });
   return (
     <div className="page">
       <div className="card">
         <div>
-          <button>Voltar</button>
+          <button onClick={() => setTask("")}>Voltar</button>
         </div>
-        {/* {Object.keys(myTasks).map((item) => (
+        {Object.keys(myTasks).map((item) => (
           <button 
             key={item}
             onClick={() => setTask(item)}
           >{item}</button>
         ))}
         <div className="content">
-          { myTasks[task] }
-        </div> */}
+          {task && <>{myTasks[task]}</>}
+        </div>
       </div>
     </div>
   );
